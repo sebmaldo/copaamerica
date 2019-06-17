1cfdc9912b704b584d1d9be378d98cbf1dabaf3d
Sebastian Maldonado <sebastian@becual.com>
2019-06-16 20:54:58 -0400

    Get first places helper

--- app/src/main/java/com/unab/copaamerica/helpers/FirebaseHelper.java
+++ app/src/main/java/com/unab/copaamerica/helpers/FirebaseHelper.java
@@ -7,4 +7,10 @@ public class FirebaseHelper {
     public static void saveToFirebase(List<Country> toSave) {
         //TODO: Acá va el código de conexión a Firebase para salvar los lugares.
     }
+
+    public static List<Country> getFirstPlaces(){
+        //TODO: Acá va el código de conexión a Firebase para bajar los 3 primeros lugares.
+        //Ojalá que utilice las preferencias para ir a buscar.
+        return null;
+    }
 }
