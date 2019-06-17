e69380a9281894898d275558aa036dafb2cec7af
Sebastian Maldonado <sebastian@becual.com>
2019-06-17 00:19:13 -0400

    Add in memory persist data

--- app/src/main/java/com/unab/copaamerica/helpers/FirebaseHelper.java
+++ app/src/main/java/com/unab/copaamerica/helpers/FirebaseHelper.java
@@ -3,17 +3,30 @@ package com.unab.copaamerica.helpers;
 import com.unab.copaamerica.model.Country;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class FirebaseHelper {
+    private static ArrayList<Country> inMemPosition = new ArrayList<>();
+
     public static void saveToFirebase(ArrayList<Country> toSave) {
         //TODO: Acá va el código de conexión a Firebase para salvar los lugares.
         //Aqui van las dos lineas para actualizar la base datos...
     }
 
+    public static void saveToFirebase(int position, Country toSave) {
+        //TODO: Acá va el código de conexión a Firebase para salvar los lugares.
+        //Aqui van las dos lineas para actualizar la base datos...
+
+        /*Sección de código para guardar en memoria por mientras en ram*/
+        inMemPosition.add(position, toSave);
+    }
+
     public static ArrayList<Country> getFirstPlaces(){
         //TODO: Acá va el código de conexión a Firebase para bajar los 3 primeros lugares.
         //Ojalá que utilice las preferencias para ir a buscar.
+
+        /*Inicio de la sección de código para tener datos*/
+        /*Esta sección de código es para tener datos, se debe eliminar al tener implementado
+        * firebase, debe en retornar un array list de 3 posiciones.*/
         ArrayList<Country> salida = new ArrayList<>();
         salida.add(new Country("Argentina",
                 "ARG",
@@ -28,6 +41,12 @@ public class FirebaseHelper {
                 "",
                 "9%"));
         salida.add(null);
-        return salida;
+
+        if(inMemPosition.size()==0){
+            inMemPosition = salida;
+        }
+        /*Fin de la sección de código para tener datos*/
+
+        return inMemPosition;
     }
 }
