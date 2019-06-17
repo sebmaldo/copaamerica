554ac5ee041c46ff8e5bc3518c7636b86cbfd0ef
Sebastian Maldonado <sebastian@becual.com>
2019-06-16 20:39:29 -0400

    add firebase helper

---
+++ app/src/main/java/com/unab/copaamerica/helpers/FirebaseHelper.java
@@ -0,0 +1,10 @@
+package com.unab.copaamerica.helpers;
+
+import com.unab.copaamerica.model.Country;
+import java.util.List;
+
+public class FirebaseHelper {
+    public static void saveToFirebase(List<Country> toSave) {
+        //TODO: Acá va el código de conexión a Firebase para salvar los lugares.
+    }
+}
